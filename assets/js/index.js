6e33d0ab1cf6e9e4bcb97bfd7d00759ae47f9498
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-23 15:19:22 +0000

    Affinity water remove popups, added delay after page loads

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,42 +2,72 @@ import puppeteer from "puppeteer";
 import 'dotenv/config';
 
 const affinitywater = async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-  
-    await page.goto('https://myonlineaccount.affinitywater.co.uk/Account/Login?ReturnUrl=%2F', {
-      waitUntil: 'domcontentloaded',
-    });
-  
-    // Wait for the login form to be visible
-    await page.waitForSelector('#Email');
-  
-    // Fill in the login form
-    await page.type('#Email', process.env.AFFINITY_WATER_USERNAME);
-    await page.type('#Password', process.env.AFFINITY_WATER_PASSWORD);
-  
-    // Click the login button
-    await page.click('.btn-login');
-  
-    // Wait for navigation to complete
-    await page.waitForNavigation();
-  
-    // Examp: Extract data from the page
-    const data = await page.evaluate(() => {
-      // Add your scraping logic here
-      // For example, you can use document.querySelector to select elements and extract data
-      if(window.document.URL === "https://myonlineaccount.affinitywater.co.uk/" ){
-          // Get the value of the element
-          setTimeout(()=>{
-            let amount = document.querySelector('.site_Next_Payment_Due_value');
-            console.log("DO YOU SEE MEE: ", );
-            return ;
-          }, 5000)
-      }
-    });
-    console.log('Scraped data:', data);
-    // Close the browser
-    await browser.close();
+   // Launch Puppeteer in headless mode
+   const browser = await puppeteer.launch({ headless: false });
+   const page = await browser.newPage();
+
+  try {
+      // Navigate to the Affinity Water login page
+      await page.goto('https://myonlineaccount.affinitywater.co.uk/Account/Login?ReturnUrl=%2F', {
+          waitUntil: 'domcontentloaded',
+      });
+
+      // Wait for the login form to be visible
+      await page.waitForSelector('#Email');
+
+      // Fill in the login form with your credentials
+      await page.type('#Email', process.env.AFFINITY_WATER_USERNAME);
+      await page.type('#Password', process.env.AFFINITY_WATER_PASSWORD);
+
+      // Click the login button
+      await page.click('.btn-login');
+
+      // Wait for navigation to complete after clicking the login button
+      await page.waitForNavigation();
+
+      // Add a 10-second delay
+      await new Promise(resolve => setTimeout(resolve, 10000));
+
+      // Example: Extract data from the page
+      const data = await page.evaluate(() => {
+          // Add your scraping logic here
+          // For example, you can use document.querySelector to select elements and extract data
+
+          // Check if the URL matches the expected URL after successful login
+          if (window.document.URL === 'https://myonlineaccount.affinitywater.co.uk/') {
+              // Remove any unwanted modal
+              let modal = document.getElementById('water-quiz-modal');
+              let modalBackdrop = document.querySelector('.modal-backdrop');
+
+              if (modal) {
+                  modal.remove();
+              }
+              if (modalBackdrop) {
+                  modalBackdrop.remove();
+              }
+
+              // Get the value of the element
+              let amountElement = document.querySelector('.site_Next_Payment_Due_value');
+              if (amountElement) {
+                  return amountElement.innerText;
+              } else {
+                  console.error('Element not found. Check the selector.');
+                  return null;
+              }
+          } else {
+              console.error('Unexpected URL after login.');
+              return null;
+          }
+      });
+
+      // Log the scraped data
+      console.log('Scraped data:', data);
+  } catch (error) {
+      console.error('An error occurred:', error);
+  } finally {
+      // Close the browser
+      await browser.close();
+  }
 };
 
 // Start the login and scraping process
