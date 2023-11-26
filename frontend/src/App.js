11c61f7244f639c9d9003af2af36b1436efb49f4
Gurjeet Singh <gsgghotra@gmail.com>
2023-11-26 22:24:04 +0000

    frontend design logo updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import logo from './images/logo.png';
 
 function App() {
   return (
     <div className="App">
+      <nav class="navbar navbar-light">
+        <a class="navbar-brand" href="#">
+          <img src={logo} width="50" height="50" class="d-inline-block align-top" alt="" />
+          Bills Tracker
+        </a>
+    </nav>
       <header className="App-header">
         <p>
           App is still in development
